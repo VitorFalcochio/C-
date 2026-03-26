270b5379b733912b427f0726dc45271e2110594e
VitorFalcochio <pretindopix69@gmail.com>
2026-03-26 11:35:38 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+// Varáveis em C#
+
+// int: para números inteiros
+
+int num1 = 10;
+int num2 = 20;
+Console.WriteLine("The sum of num1 and num2 is: " + (num1 + num2));
+
+// double: para números decimais
+double num3 = 5.5;
+double num4 = 2.3;
+Console.WriteLine("The product of num3 and num4 is: " + (num3 * num4));
+
+// char: para caracteres individuais
+char letter = 'A';
+Console.WriteLine("The character is: " + letter);
+
+// string: para texto
+string name = "John";
+Console.WriteLine("Hello, " + name + "!");
+
+// bool: para valores booleanos (true ou false)
+bool isRaining = true;
+
+// Exibindo o valor da variável booleana
+Console.WriteLine("Is it raining? " + isRaining);
